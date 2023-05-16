8ce6eee24a892e0738f9a78afe6ede0184b1e4cf
Han <hander.juan@gmail.com>
2023-05-16 19:36:54 +0200

    finish funcionaltity

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,59 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { atom, useSetAtom, useAtom } from "jotai";
 import type { PrimitiveAtom } from "jotai";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 type Todo = {
   title: string;
 };
+type RemoveFn = (todo: PrimitiveAtom<Todo>) => void;
 
 const todosAtom = atom<PrimitiveAtom<Todo>[]>([]);
+
+function TodoItem({
+  todoAtom,
+  remove,
+}: {
+  todoAtom: PrimitiveAtom<Todo>;
+  remove: RemoveFn;
+}) {
+  const [todo] = useAtom(todoAtom);
+  return (
+    <div
+      className="
+      flex
+      w-full
+      max-w-sm
+      items-center
+      space-x-2
+      mt-4
+      border
+      border-gray-200
+      rounded-md
+      p-2
+      justify-between
+    "
+    >
+      <span className="text-gray-700">{todo.title}</span>
+      <Button
+        type="button"
+        variant="destructive"
+        onClick={() => remove(todoAtom)}
+      >
+        Remove
+      </Button>
+    </div>
+  );
+}
 
 export default function Home() {
   const setTodos = useSetAtom(todosAtom);
   const [todos] = useAtom(todosAtom);
+  const [parent] = useAutoAnimate();
+
+  const remove: RemoveFn = (todo) => {
+    setTodos((prev) => prev.filter((t) => t !== todo));
+  };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -35,8 +78,12 @@ export default function Home() {
           <Input type="text" placeholder="Title" name="title" />
           <Button type="submit">Add</Button>
         </div>
-        {JSON.stringify(todos)}
       </form>
+      <ul ref={parent}>
+        {todos.map((todo) => (
+          <TodoItem key={todo.toString()} todoAtom={todo} remove={remove} />
+        ))}
+      </ul>
     </main>
   );
 }
